514fa3ce7f98e01d8addf9e33e05ab8c12e0163d
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-09-12 20:52:23 +0500

    Create App & Install Packages

--- build/index.js
+++ build/index.js
@@ -1,2 +1,20 @@
-console.log("Node.JS Express TypeScript PostgreSQL (OOP Backend) With Swagger UI");
-export {};
+import express from "express";
+class App {
+    app;
+    constructor() {
+        this.app = express();
+    }
+    ;
+    routes() {
+        this.app.route("/").get((req, res) => {
+            res.send("Welcome Express App");
+        });
+    }
+    ;
+}
+;
+const port = 8000;
+const app = new App().app;
+app.listen(port, () => {
+    console.log("Server Successfully Started!");
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Node.JS Express TypeScript PostgreSQL (OOP Backend) With Swagger UI");
+import express, { Application, Request, Response } from "express";
+
+class App {
+    public app: Application;
+
+    constructor() {
+        this.app = express();
+    };
+
+    protected routes(): void {
+        this.app.route("/").get((req: Request, res: Response) => {
+            res.send("Welcome Express App");
+        });
+    };
+};
+
+const port: number = 8000;
+
+const app = new App().app;
+
+app.listen(port, () => {
+    console.log("Server Successfully Started!");
+});
